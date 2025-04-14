6889d923f5292965563bb40b4ac3a89907a6413b
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-04-14 22:08:47 +0300

    implement encapsulation and inheritance

--- src/csie/ase/ro/classes/Lander.java
+++ src/csie/ase/ro/classes/Lander.java
@@ -1,9 +1,64 @@
 package csie.ase.ro.classes;
 
-public class Lander {
+import java.util.Arrays;
+
+public class Lander extends Spacecraft {
 
     private String landingSite;
     private boolean hasLanded; // shows if the lander has landed; by default, this should be FALSE
     int[] temperatureReadings; // stores the temperatures registered until now
 
+    public Lander() {
+        super();
+        this.landingSite = "";
+        this.hasLanded = false;
+        this.temperatureReadings = null;
+    }
+
+    public Lander(String landingSite, boolean hasLanded, int[] temperatureReadings) {
+        this.landingSite = landingSite;
+        this.hasLanded = hasLanded;
+        this.temperatureReadings = temperatureReadings; // change to system.arraycopy
+    }
+
+    public Lander(String name, float weight, String landingSite, boolean hasLanded, int[] temperatureReadings) {
+        super(name, weight);
+        this.landingSite = landingSite;
+        this.hasLanded = hasLanded;
+        this.temperatureReadings = temperatureReadings; // change to system.arraycopy
+    }
+
+    public String getLandingSite() {
+        return landingSite;
+    }
+
+    public void setLandingSite(String landingSite) {
+        this.landingSite = landingSite;
+    }
+
+    public boolean isHasLanded() {
+        return hasLanded;
+    }
+
+    public void setHasLanded(boolean hasLanded) {
+        this.hasLanded = hasLanded;
+    }
+
+    public int[] getTemperatureReadings() {
+        return temperatureReadings; // change this aswell
+    }
+
+    public void setTemperatureReadings(int[] temperatureReadings) {
+        this.temperatureReadings = temperatureReadings; // change this aswell
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Lander{");
+        sb.append("landingSite='").append(landingSite).append('\'');
+        sb.append(", hasLanded=").append(hasLanded);
+        sb.append(", temperatureReadings=").append(Arrays.toString(temperatureReadings));
+        sb.append('}');
+        return sb.toString();
+    }
 }
--- src/csie/ase/ro/classes/Spacecraft.java
+++ src/csie/ase/ro/classes/Spacecraft.java
@@ -5,4 +5,38 @@ public class Spacecraft {
     private String name;
     private float weight; // measured in kg
 
+    public Spacecraft() {
+        this.name = "";
+        this.weight = -1000;
+    }
+
+    public Spacecraft(String name, float weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Spacecraft{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", weight=").append(weight);
+        sb.append('}');
+        return sb.toString();
+    }
 }
